c29da4342a7795deb2b519e297313cfbab77f3ba
harris2001 <harrishadjiantonis@gmail.com>
2024-08-04 18:08:47 +0100

    Testing KANs on toy datasets

--- Experiments/Split_MNIST/synaptic_intelligence.py
+++ Experiments/Split_MNIST/synaptic_intelligence.py
@@ -5,6 +5,7 @@ from torch.optim import Adam
 from avalanche.evaluation import metrics as metrics
 # from models import MultiHeadMLP
 
+import numpy as np
 import os 
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '../', '../', 'Utils')))
@@ -16,6 +17,7 @@ import os
 # from Scaled_KAN import *
 
 from kan import KAN
+
 
 def synaptic_intelligence_smnist(override_args=None):
     """
@@ -51,24 +53,36 @@ def synaptic_intelligence_smnist(override_args=None):
         device=device, evaluator=evaluation_plugin)
 
     dataset = {}
-    for experience in benchmark.train_stream:
-        train_input = []
-        train_label = []
-        for (x,y,t) in experience.dataset:
-            train_input.append(x)
-            train_label.append(y)
-        dataset['train_input'] = train_input
-        dataset['train_label'] = train_label
-        model.fit(dataset, opt = 'LBFGS', steps=10, update_grid=True)
-        # model.train(dataset=dataset, epochs=10, batch_size=64, opt="Adam", lr=0.001)
+
+    def train_acc():
+        return torch.mean((torch.argmax(model(dataset['train_input']), dim=1) == dataset['train_label']).float())
+
+    def test_acc():
+        return torch.mean((torch.argmax(model(dataset['test_input']), dim=1) == dataset['test_label']).float())
+
     for experience in benchmark.test_stream:
         test_input = []
         test_label = []
         for (x,y,t) in experience.dataset:
             test_input.append(x)
             test_label.append(y)
-        dataset['test_input'] = test_input
-        dataset['test_label'] = test_label
+        dataset['test_input'] = torch.from_numpy(np.array(test_input))
+        dataset['test_label'] = torch.from_numpy(np.array(test_label))
+
+    for experience in benchmark.train_stream:
+        train_input = []
+        train_label = []
+        for (x,y,t) in experience.dataset:
+            train_input.append(x)
+            train_label.append(y)
+        dataset['train_input'] = torch.from_numpy(np.array(train_input))
+        dataset['train_label'] = torch.from_numpy(np.array(train_label))
+        print(dataset['train_input'].shape)
+        print(dataset['train_label'].shape)
+        print(dataset['test_input'].shape)
+        print(dataset['test_label'].shape)
+        # model.train(dataset=dataset, epochs=10, batch_size=64, opt="Adam", lr=0.001)
+        model.fit(dataset, opt = 'LBFGS', steps=10, loss_fn=CrossEntropyLoss(), metrics=[train_acc, test_acc], display_metrics=['train_loss', 'reg', 'train_acc', 'test_acc'], update_grid=False)
 
     res = cl_strategy.eval(benchmark.test_stream)
     res = 0
